851fdcf63bcd37da2f4a537d71ab0d7640488b60
이영곤 <guest110@skylabs.io>
2025-12-09 17:08:22 +0900

    Initial commit

---
+++ android/app/src/main/kotlin/com/gondev/app/cach_wallets/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.gondev.app.cach_wallets
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,142 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+      ),
+      home: const MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      backgroundColor: Color(0xff181818),
+      body: Padding(
+        padding: EdgeInsets.all(16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            SizedBox(height: 50),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.end,
+                  children: [
+                    Text(
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 38,
+                        fontWeight: FontWeight.bold,
+                      ),
+                      "Hey, Selena",
+                    ),
+                    Text(
+                      style: TextStyle(
+                        color: Colors.white54,
+                        fontSize: 24,
+                        fontWeight: FontWeight.bold,
+                      ),
+                      "Welcom back",
+                    ),
+                  ],
+                ),
+              ],
+            ),
+            SizedBox(height: 120),
+            Text(
+              style: TextStyle(
+                color: Colors.white54,
+                fontSize: 24,
+                fontWeight: FontWeight.bold,
+              ),
+              "Total Balance",
+            ),
+            Text(
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 38,
+                fontWeight: FontWeight.bold,
+              ),
+              "\$5 194 482",
+            ),
+            SizedBox(height: 20),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Button(
+                  name: "Transfer",
+                  color: Color(0xfff2833a),
+                  textColor: Colors.black,
+                ),
+                Button(
+                  name: "Request",
+                  color: Colors.white10,
+                  textColor: Colors.white,
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class Button extends StatelessWidget {
+  final String name;
+
+  final Color color;
+
+  final Color textColor;
+
+  const Button({
+    super.key,
+    required this.name,
+    this.color = Colors.white,
+    this.textColor = Colors.black,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.all(12),
+      decoration: BoxDecoration(
+        color: color,
+        borderRadius: BorderRadius.circular(40),
+      ),
+      child: Padding(
+        padding: const EdgeInsets.symmetric(vertical: 15, horizontal: 15),
+        child: Row(
+          children: [
+            Icon(Icons.currency_bitcoin, color: textColor),
+            const SizedBox(width: 8),
+            Text(
+              style: TextStyle(
+                color: textColor,
+                fontSize: 18,
+                fontWeight: FontWeight.bold,
+              ),
+              name,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
